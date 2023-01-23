9ea93a37bb298fbf10a995d69436495171237146
ulassekerci <107992457+ulassekerci@users.noreply.github.com>
2023-01-23 21:44:25 +0300

    added page title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
 import '../tailwind.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Reverse QR</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
